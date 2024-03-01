a8bb7f76c9253b22286e998fbdb91ce6d8e30352
Arya <aryasolhi@gmail.com>
2024-02-29 21:17:11 -0500

    Adds example logs for representative in-browser view and fixes minor issues (#2)
    * adds example logs for browser previews and updates styles
    
    * adds styled nav tabs
    
    * scrolls to bottom of logs if was at bottom before new log was added.
    
    * checks for valid timestamp before adding colors and adds element keys to avoid unnecessary rerenders
    
    ---------
    
    Co-authored-by: Automated Release Test <release-tests-no-reply@zfnd.org>

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,51 @@
-import { createSignal, onCleanup, onMount } from "solid-js";
 import { listen, Event, UnlistenFn } from "@tauri-apps/api/event";
 
+import { createSignal, onCleanup, onMount, For } from "solid-js";
 import { styled } from "solid-styled-components";
+
+import { EXAMPLE_LOGS } from "./tests/example_logs";
+
+const NAVIGATION_BAR_HEIGHT = "50px";
+
+const TabNavigation = styled("ul")`
+  display: flex;
+  position: fixed;
+  font-size: 12px;
+  background-color: #1c1c1c;
+  margin: 0;
+  height: ${NAVIGATION_BAR_HEIGHT};
+  width: 100%;
+  box-sizing: border-box;
+  list-style-type: none;
+  padding: 0 0 12px;
+`;
+
+const TabItem = styled("li")`
+  text-transform: uppercase;
+  font-family: sans-serif;
+  padding: 16px 4px 6px;
+  margin: 0 20px;
+  vertical-align: middle;
+  border-bottom: solid 1px #fff;
+`;
+
+const LogPage = styled("div")`
+  display: flex;
+  flex-grow: 1;
+  flex-direction: column;
+  overflow-x: hidden;
+  overflow-y: hidden;
+  background-color: #1c1c1c;
+`;
 
 const LogContainer = styled("div")`
   display: flex;
   flex-grow: 1;
   flex-direction: column;
   justify-content: end;
-  overflow-x: hidden;
-  overflow-y: scroll;
   background-color: #1c1c1c;
-  padding: 8px;
+  padding: 4px;
+  margin-top: ${NAVIGATION_BAR_HEIGHT};
 `;
 
 const LogLine = styled("div")`
@@ -46,36 +80,87 @@ const LogMessage = styled("code")`
   color: #eee;
 `;
 
-const Log = ({ children }: { children: string }) => (
-  <LogLine>
-    <LogTimestamp>{children.slice(0, 27)}</LogTimestamp>
-    <LogLevel>{children.slice(27, 34)}</LogLevel>
-    <LogMessage>{children.slice(34)}</LogMessage>
-  </LogLine>
-);
+const Log = ({ children }: { children: string }) => {
+  const is_likely_timestamp = /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{6}Z/.test(
+    children.slice(0, 27)
+  );
+
+  if (is_likely_timestamp) {
+    return (
+      <LogLine>
+        <LogTimestamp>{children.slice(0, 27)}</LogTimestamp>
+        <LogLevel>{children.slice(27, 34)}</LogLevel>
+        <LogMessage>{children.slice(34)}</LogMessage>
+      </LogLine>
+    );
+  } else {
+    return (
+      <LogLine>
+        <LogMessage>{children}</LogMessage>
+      </LogLine>
+    );
+  }
+};
 
 function App() {
-  let stop_listening: UnlistenFn;
-
+  const is_tauri_app = window.hasOwnProperty("__TAURI_INTERNALS__");
   const [logs, set_logs] = createSignal<Array<string>>([]);
 
-  onMount(async () => {
-    stop_listening = await listen("log", (event: Event<string>) =>
-      set_logs([...logs(), event.payload])
-    );
-  });
+  const is_at_bottom = () => {
+    const y_bottom = Math.ceil(window.scrollY) + window.innerHeight;
+    return y_bottom >= document.body.scrollHeight;
+  };
 
-  onCleanup(() => stop_listening());
+  const scroll_to_bottom = () => {
+    window.scroll(0, document.body.scrollHeight);
+  };
+
+  if (is_tauri_app) {
+    let stop_listening: UnlistenFn;
+
+    onMount(async () => {
+      stop_listening = await listen("log", (event: Event<string>) => {
+        const was_at_bottom = is_at_bottom();
+
+        set_logs([...logs(), event.payload]);
+
+        if (was_at_bottom) {
+          scroll_to_bottom();
+        }
+      });
+    });
+
+    onCleanup(() => stop_listening());
+  } else {
+    let example_log_index = 0;
+    const interval = setInterval(() => {
+      const was_at_bottom = is_at_bottom();
+
+      set_logs([...logs(), EXAMPLE_LOGS[example_log_index]]);
+      example_log_index += 1;
+
+      if (was_at_bottom) {
+        scroll_to_bottom();
+      }
+
+      if (example_log_index >= EXAMPLE_LOGS.length) {
+        clearInterval(interval);
+      }
+    }, 150);
+  }
 
   return (
-    <>
+    <LogPage>
+      <TabNavigation>
+        <TabItem>Logs</TabItem>
+      </TabNavigation>
       <LogContainer>
-        {logs().map((log) => (
-          <Log>{log}</Log>
-        ))}
+        <For each={logs()} fallback={<Log>Waiting for zebrad to start...</Log>}>
+          {(log) => <Log>{log}</Log>}
+        </For>
         <LogLine>&gt; _</LogLine>
       </LogContainer>
-    </>
+    </LogPage>
   );
 }
 
