fd589615ef489ac2f2e3a1d2addfe539d40e1fe3
Arya <aryasolhi@gmail.com>
2024-03-04 14:04:01 -0500

    adds zebrad as a bundled external binary (#4)
    Co-authored-by: Automated Release Test <release-tests-no-reply@zfnd.org>

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -10,11 +10,9 @@ use std::{
 
 use tauri::{AppHandle, Manager, RunEvent};
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+// TODO: Add a command for updating the config and restarting `zebrad` child process
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+fn save_config() {}
 
 fn main() {
     // Spawn initial zebrad process
@@ -27,6 +25,9 @@ fn main() {
     // Spawn a task for reading output and sending it to a channel
     let (zebrad_log_sender, mut zebrad_log_receiver) = tokio::sync::mpsc::channel(100);
     let zebrad_stdout = zebrad.stdout.take().expect("should have anonymous pipe");
+
+    // TODO: Use a blocking tokio/async_runtime thread? The io is blocking (reading the child process output from stdio), so
+    //       it shouldn't use a green thread
     let _log_emitter_handle = std::thread::spawn(move || {
         for line in BufReader::new(zebrad_stdout).lines() {
             // Ignore send errors for now
@@ -48,7 +49,7 @@ fn main() {
 
             Ok(())
         })
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![save_config])
         .build(tauri::generate_context!())
         .unwrap()
         .run(move |app_handle: &AppHandle, _event| {
